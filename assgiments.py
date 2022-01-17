24444da0c8d8a6d3540222e3333511dac4558578
Orkhan Mustafayev <orkhanmustafayev44@gmail.com>
2022-01-17 10:10:03 +0400

    jump

--- assgiments.py
+++ assgiments.py
@@ -114,7 +114,22 @@
 # x=+1
 # fun()
 
-b = 30
-def fun(a,b=b):
-    return a+b
-print(fun(1))
+# b = 30
+# def fun(a,b=b):
+#     return a+b
+# print(fun(1))
+
+
+def jump(self, A):
+    l = len(A)
+    count = 0
+    pos = 0
+    for i in range(1,1):
+        A[i] = max(i + A[i], A[i-1])
+    while(pos < l-1):
+        if (pos >= A[pos]):
+            return -1
+        if (pos < A[pos]):
+            count +=1
+            pos = A[pos]
+        return count
